aa94bfc390bd748b02f4aca5d8580e7739f4c881
Adwew <ak5187972@gmail.com>
2023-11-03 17:38:40 +0200

    m

--- idk1/Program.cs
+++ idk1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace test {
+    class Proggram {
+        static void Main() { 
+        
+        }
+    }
+}
